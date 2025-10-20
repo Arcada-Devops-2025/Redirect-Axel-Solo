9a894939aaaaa32fa4c4f05bbfaea1f4f1704960
Axe-Blomq <blomqaxe@arcada.fi>
2025-10-20 11:36:49 +0300

    implemented the Serious links

--- redirectors/SeriousRedirect.js
+++ redirectors/SeriousRedirect.js
@@ -8,35 +8,61 @@ export const handler = async (event) => {
     case '1':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say>Testing, Jukebox Bot</Say>
-  <Redirect method="POST">http://jukebox-env.eba-vvgs5ded.eu-north-1.elasticbeanstalk.com/</Redirect>
+  <Redirect method="GET">https://6kk5wvtwaiddg6tnwiqdo5njsm0peydx.lambda-url.eu-north-1.on.aws/</Redirect>
 </Response>`;
       break;
     case '2':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say>Testing, Trivia Bot</Say>
-  <Redirect method="POST">https://devops-twiliobot-tasthu-1.onrender.com</Redirect>
+  <Redirect method="POST">https://2nuxjt6m2a.execute-api.eu-north-1.amazonaws.com/twilio</Redirect>
 </Response>`;
       break;
     case '3':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say>Testing, Dad Joke Bot</Say>
-  <Redirect method="POST">https://lr9i3ac02k.execute-api.eu-north-1.amazonaws.com/voice</Redirect>
+  <Redirect method="POST">https://devops-twilio-voicebot.onrender.com/webhook</Redirect>
 </Response>`;
       break;
     case '4':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say>Testing, BLANK Bot</Say>
-  <Redirect method="POST"><!-- web adress goes here --></Redirect>
+  <Redirect method="GET">http://ec2-13-60-40-185.eu-north-1.compute.amazonaws.com/</Redirect>
+</Response>`;
+      break;
+    case '5':
+      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Redirect method="POST">https://yverqcsiuvj3vqgtedkwnpaqum0hqnqd.lambda-url.eu-north-1.on.aws/voice</Redirect>
+</Response>`;
+      break;
+    case '6':
+      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Redirect method="POST">https://dev-ops-projekt-arcada-devops.2.rahtiapp.fi/bot</Redirect>
+</Response>`;
+      break;
+    case '7':
+      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Redirect method="GET">http://ec2-13-62-103-150.eu-north-1.compute.amazonaws.com</Redirect>
+</Response>`;
+      break;
+    case '8':
+      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Redirect method="POST">http://ec2-3-85-143-252.compute-1.amazonaws.com/voicecall</Redirect>
+</Response>`;
+      break;
+    case '9':
+      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Redirect method="POST">https://tlcd6qc2djxftr4aiwuf4hjqvy0bnwhh.lambda-url.us-east-1.on.aws/</Redirect>
 </Response>`;
       break;
     default:
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say>Joke dot Invalid number, try again.</Say>
+  <Say>Invalid number, try again.</Say>
   <Redirect method="GET">http://testforexersise.s3-website.eu-north-1.amazonaws.com/mainCode/menu.xml</Redirect>
 </Response>`;
   }
