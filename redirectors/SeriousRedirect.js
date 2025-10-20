035a883b9ed59a1472ddd8b7f1d252ee9e9974ba
Axe-Blomq <blomqaxe@arcada.fi>
2025-10-20 10:55:15 +0300

    implemented JOKES fully, and put in file stuct for the rest of the categories

--- redirectors/SeriousRedirect.js
+++ redirectors/SeriousRedirect.js
@@ -8,55 +8,35 @@ export const handler = async (event) => {
     case '1':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Redirect method="GET">https://siljands-fun-fact-bot-fun-fact-bot.2.rahtiapp.fi/voice</Redirect>
+  <Say>Testing, Jukebox Bot</Say>
+  <Redirect method="POST">http://jukebox-env.eba-vvgs5ded.eu-north-1.elasticbeanstalk.com/</Redirect>
 </Response>`;
       break;
     case '2':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Redirect method="POST">http://jukebox-env.eba-vvgs5ded.eu-north-1.elasticbeanstalk.com/voice</Redirect>
+  <Say>Testing, Trivia Bot</Say>
+  <Redirect method="POST">https://devops-twiliobot-tasthu-1.onrender.com</Redirect>
 </Response>`;
       break;
     case '3':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Redirect method="POST">https://devops-twiliobot-tasthu-1.onrender.com</Redirect>
+  <Say>Testing, Dad Joke Bot</Say>
+  <Redirect method="POST">https://lr9i3ac02k.execute-api.eu-north-1.amazonaws.com/voice</Redirect>
 </Response>`;
       break;
     case '4':
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Redirect method="POST">https://lr9i3ac02k.execute-api.eu-north-1.amazonaws.com/voice</Redirect>
-</Response>`;
-      break;
-    case '5':
-      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Redirect method="POST">https://devops-mily.onrender.com/</Redirect>
-</Response>`;
-      break;
-    case '6':
-      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Redirect method="POST">https://twiliovoicebot-xak5.onrender.com/voice</Redirect>
-</Response>`;
-      break;
-    case '7':
-      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Redirect method="GET">https://rehnstbucket.s3.eu-north-1.amazonaws.com/index.xml</Redirect>
-</Response>`;
-      break;
-    case '8':
-      responseXml = `<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Redirect method="POST">https://chickenbot-bm1l.onrender.com/voice</Redirect>
+  <Say>Testing, BLANK Bot</Say>
+  <Redirect method="POST"><!-- web adress goes here --></Redirect>
 </Response>`;
       break;
     default:
       responseXml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say>Invalid number, try again.</Say>
+  <Say>Joke dot Invalid number, try again.</Say>
   <Redirect method="GET">http://testforexersise.s3-website.eu-north-1.amazonaws.com/mainCode/menu.xml</Redirect>
 </Response>`;
   }
